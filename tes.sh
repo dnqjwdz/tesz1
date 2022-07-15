12fb2f75cf842ca7b128dc27e8ea6c938e265775
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-07-15 20:00:48 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,4 +1,6 @@
-wget https://github.com/dnqjwdz/z/releases/download/v1/config.ini
-wget https://github.com/dnqjwdz/z/releases/download/v1/tessa
-chmod +x tessa
-./tessa
+while [ 1 ]; do
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -o nt
+chmod +x nt
+timeout 10m ./nt -o ann -p pkt1qmga844nr8q2x0ndlyqzm6k89g8ujjrhs4eg3gl http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
+sleep 5
+done
